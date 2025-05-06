e04d0da84d76953f80f43e2668908031849248e4
AngieNatalia01 <angie123nata@gmail.com>
2025-05-06 08:08:40 -0500

    Add files via upload

---
+++ Corte 1/Tarea 3/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Ingrese el primer número: ");
+            int numero1 = scanner.nextInt();
+
+            System.out.print("Ingrese el segundo número: ");
+            int numero2 = scanner.nextInt();
+
+            Operations operacion = new Operations(numero1, numero2);
+
+            // Mostramos los resultados de las operaciones
+            System.out.println("Suma: " + operacion.sumar());
+            System.out.println("Resta: " + operacion.restar());
+            System.out.println("Multiplicación: " + operacion.multiplicar());
+        }
+    }
+}
+
+
+
