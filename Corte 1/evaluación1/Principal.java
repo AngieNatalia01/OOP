08d5d0bcfde16df3414db9cbfa4bb7197384fc3f
AngieNatalia01 <angie123nata@gmail.com>
2025-05-13 17:41:45 -0500

    Delete Corte 1/evaluación1

--- Corte 1/evaluación1/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-
-    public static void main(String[] args) {
-        // Crear una instancia de la interfaz gráfica del cajero automático
-        Interfaz interfaz = new Interfaz();
-        interfaz.setVisible(true);
-    }
-
-}
